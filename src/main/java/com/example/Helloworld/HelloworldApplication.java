9ba96c6ad5467282b516715a51d7efe8fdb6c575
coderuma <coderumapathi@gmail.com>
2023-07-29 13:46:09 +0530

    Helloworld

--- src/main/java/com/example/Helloworld/HelloworldApplication.java
+++ src/main/java/com/example/Helloworld/HelloworldApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+	}
+	
+	@RequestMappaing(value = "/")
+	public String hello()
+	{
+		return "Hello World";
 	}
 
 }
