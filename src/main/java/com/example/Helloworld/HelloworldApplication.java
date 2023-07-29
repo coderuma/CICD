04f397f1732f14145173410c291ae1106139d54a
coderuma <coderumapathi@gmail.com>
2023-07-29 13:50:13 +0530

    Done

--- src/main/java/com/example/Helloworld/HelloworldApplication.java
+++ src/main/java/com/example/Helloworld/HelloworldApplication.java
@@ -2,7 +2,8 @@ package com.example.Helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annonation.RequestMapping;
+import org.springframework.web.bind.annonation.ResController;
 @SpringBootApplication
 @RestController
 public class HelloworldApplication {
