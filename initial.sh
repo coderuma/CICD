c67860b5d9d1a0ee2414cc3d95769c4d6397cb84
coderuma <112417279+coderuma@users.noreply.github.com>
2023-08-15 11:27:44 +0530

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -2,7 +2,7 @@
 mkdir -p /root/python-webAPP
 rm -Rf /root/python-webAPP/*
 rm -f *.zip *.sh
-unzip /var/www/html/*.zip -d /root/python-WebAPP
-python3 -m vevn env
+unzip /var/www/html/*.zip -d /root/python-webAPP
+python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
